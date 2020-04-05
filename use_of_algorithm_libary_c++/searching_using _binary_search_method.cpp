f079b0c678e3117eb5c3d17dbc835092c6f5c745
Himanshu Jain <49681798+himanshujainpro@users.noreply.github.com>
2020-04-05 21:43:46 +0530

    Create searching_using _binary_search_method.cpp

---
+++ use_of_algorithm_libary_c++/searching_using _binary_search_method.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+// demo of searching in the array using inbuilt c++ library!
+// using binary_search method
+int main(){
+    int a[10]={9,8,7,6,5,4,3,2,1,10};
+    int i;
+
+    // starting and ending address of the array, and element to be searched!
+    if(binary_search(a,a+10,10)) cout<<"Yes Present";
+    else cout<<"Not present";
+
+    cout<<endl;
+
+    if(binary_search(a,a+10,0)) cout<<"Yes Present";
+    else cout<<"Not present";
+
+    cout<<endl;
+}
